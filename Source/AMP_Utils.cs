b6aa6f2212e4d696a70f393de0519193a6ba61da
Merwini <86023141+Merwini@users.noreply.github.com>
2024-03-12 09:40:41 -0400

    Split FindAMPSites into FindAMPSitesFor and FindAMPSiteDefsFor

--- Source/AMP_Utils.cs
+++ Source/AMP_Utils.cs
@@ -13,31 +13,58 @@ namespace nuff.ArmyModernizationProject
 {
     public static class AMP_Utils
     {
-        //TODO rewrite this to return the sites themselves, and a second method to return the defs from that
-        public static List<SitePartDef> FindAmpSites()
+        public static List<Faction> ReturnAMPFactions()
         {
-            List<SitePartDef> activeAmpSitePartDefs = new List<SitePartDef>();
+            List<Faction> amps = Find.FactionManager.AllFactions.Where(f => (f.def == AMP_DefOf.AMP_FactionFriendly || f.def == AMP_DefOf.AMP_FactionHostile)).ToList();
+
+            return amps;
+        }
+
+        public static List<Faction> ReturnHostileAMPFactions()
+        {
+            List<Faction> hamps = ReturnAMPFactions().Where(f => f.HostileTo(Find.FactionManager.OfPlayer)).ToList();
+
+            return hamps;
+        }
+
+        public static List<Site> FindAMPSitesFor(Faction fact)
+        {
+            List<Site> ampSites = new List<Site>();
 
             List<Site> worldSites = Find.WorldObjects.Sites;
             foreach (Site site in worldSites)
             {
                 if (site == null)
-                    break;
-
-                SitePartDef mainSitePartDef = site.MainSitePartDef;
-                if (mainSitePartDef == null)
-                    break;
-
-                foreach (string tag in mainSitePartDef.tags)
+                    continue;
+                if (site.Faction != fact)
+                    continue;
+                if (site.MainSitePartDef == null)
+                    continue;
+                if (site.MainSitePartDef.tags.Any(t => t.StartsWith("AMP_")))
                 {
-                    if (tag.StartsWith("amp_"))
-                    {
-                        activeAmpSitePartDefs.Add(mainSitePartDef);
-                    }
+                    ampSites.Add(site);
+                    continue;
                 }
             }
 
-            return activeAmpSitePartDefs;
+            return ampSites;
+        }
+
+        public static List<SitePartDef> FindAMPSiteDefsFor(Faction fact)
+        {
+            List<SitePartDef> ampSitePartDefs = new List<SitePartDef>();
+
+            List<Site> ampSites = FindAMPSitesFor(fact);
+
+            if (!ampSites.NullOrEmpty())
+            {
+                foreach (Site site in ampSites)
+                {
+                    ampSitePartDefs.Add(site.MainSitePartDef);
+                }
+            }
+
+            return ampSitePartDefs;
         }
     }
 }
